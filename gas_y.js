17c04e55427727c34bb7a8294bd920aa7b2da0dd
longtime1116 <longtime1116@gmail.com>
2019-05-06 01:21:26 +0900

    update gas_y.js

--- gas_y.js
+++ gas_y.js
@@ -1,8 +1,32 @@
+function mcv_cv_of(value_hash, sheetString){
+  const sheet  = ss.getSheetByName(sheetString);
+  const tgRowFrom = 3;
+  const tgColFrom = 7;
+  const tgRowCount = 100;
+  //const tgRowCount = sheet.getMaxRows();
+  const tgColCount = 15;
+  //const tgColCount = sheet.getMaxColumns();
+ 
+  const targets = sheet.getSheetValues(tgRowFrom, tgColFrom, tgRowCount, tgColCount);
+  
+  var sums = [];
+  targets.forEach(function(keys){
+    var sum = [0, 0];
+    keys.forEach(function(key){
+      if (value_hash[key]) {
+        sum[0]+=value_hash[key][0];
+        sum[1]+=value_hash[key][1];
+      }
+    })
+    sums.push(sum);
+  })
+  sheet.getRange("D3:E" + (3+sums.length-1).toString()).setValues(sums);
+  return;
+}
 function mcv_cv() {
   //シートオブジェクトを取得
   const ss     = SpreadsheetApp.getActiveSpreadsheet();
   const sheet1  = ss.getSheetByName("webantenna");
-  const sheet2  = ss.getSheetByName("バーム紐づけ");
   
   const vRowFrom = 2;
   const vColFrom = 1;
@@ -10,18 +34,10 @@ function mcv_cv() {
   //const vRowCount = sheet1.getMaxRows();
   const vColCount = 3;
   
-  const tgRowFrom = 3;
-  const tgColFrom = 7;
-  const tgRowCount = 100;
-  //const tgRowCount = sheet2.getMaxRows();
-  const tgColCount = 15;
-  //const tgColCount = sheet2.getMaxColumns();
-  
   
   //セルの内容を2次元配列に格納
   const values = sheet1.getSheetValues(vRowFrom, vColFrom, vRowCount, vColCount);
-  const targets = sheet2.getSheetValues(tgRowFrom, tgColFrom, tgRowCount, tgColCount);
-  
+
   // NOTE: value_hash とは?
   // key: 媒体名
   // value: [<mcv>, <cv>]
@@ -36,20 +52,11 @@ function mcv_cv() {
       value_hash[value[0]] = [value[1], value[2]];
     }
   )
+
+  mcv_cv_of(value_hash, "バーム紐づけ");
+  mcv_cv_of(value_hash, "ホワイト紐づけ");
   
-  var sums = [];
-  targets.forEach(function(keys){
-    var sum = [0, 0];
-    keys.forEach(function(key){
-      if (value_hash[key]) {
-        sum[0]+=value_hash[key][0];
-        sum[1]+=value_hash[key][1];
-      }
-    })
-    sums.push(sum);
-  })
-  sheet2.getRange("D3:E" + (3+sums.length-1).toString()).setValues(sums);
-  return sums;
+  return;
 }
 
 
